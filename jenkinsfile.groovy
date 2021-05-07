8ff879f40551292a35fa08b9b6ba2aba303f5ccb
groupe2sopra <83815239+groupe2sopra@users.noreply.github.com>
2021-05-07 13:01:23 +0200

    Create jenkinsfile.groovy

---
+++ jenkinsfile.groovy
@@ -0,0 +1,14 @@
+node{
+  cleanWs()
+  try{
+    stage('PremiereEtape'){
+      sh "echo 'Hello World' !!'"
+    }
+    stage ('DeuxiemeEtape'){
+      sh "echo 'Hello World 2 !!'"
+    }
+  }
+  finally{
+    cleanWs()
+  }
+}
