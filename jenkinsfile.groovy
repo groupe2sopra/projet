1ec4c1ad41292aefe7dad3fe8894005f33aad3ff
groupe2sopra <83815239+groupe2sopra@users.noreply.github.com>
2021-05-07 14:44:00 +0200

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -3,6 +3,8 @@ node{
   try{
     stage('PremiereEtape'){
       sh "echo 'Hello World !!'"
+      sh " cd .. "
+      sh " tar -cf test_build.tar projet"
     }
     stage ('DeuxiemeEtape'){
       sh "echo 'Hello World 2 !!'"
