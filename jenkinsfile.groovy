650502d74356e5265289e0698d7e673edd526663
groupe2sopra <83815239+groupe2sopra@users.noreply.github.com>
2021-05-07 15:30:27 +0200

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,10 +1,10 @@
 node{
-  cleanWs()
+  
   try{
     stage('PremiereEtape'){
       sh "echo 'Hello World !!'"
       sh "ls -al"
-      sh " cd . "
+      sh " cd .. "
       sh "tar -cf test_build.tar ."
       sh "ls -al"
     }
